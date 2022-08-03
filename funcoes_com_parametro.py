2ba8ae627ffe277b1c496a150944d898e46b266c
Gigleydson Dias <gigleydson@gmail.com>
2022-08-03 14:53:47 -0300

    Aula finalizada

--- funcoes_com_parametro.py
+++ funcoes_com_parametro.py
@@ -44,7 +44,6 @@ def cantar_parabens(aniversariante):
 
 
 cantar_parabens('Gigleydson')
-"""
 
 
 # Funções podem ter n parâmetros de entrada. Ou seja, podemos receber como entrada
@@ -73,3 +72,54 @@ print(outra(4, 6, 'Resultado '))
 
 # print(soma(2, 3, 4))  # TypeErros - Passando argumentos a mais
 # print(soma(2))  # TypeErros - Passando argumentos a menos
+
+
+# Nomeando parâmetros
+
+def nome_completo(nome, sobrenome):
+    return f'O seu nome completo é {nome} {sobrenome}'
+
+
+print(nome_completo('Gigleydson', 'Dias'))
+
+
+# A diferença entre Parâmetros e Argumentos
+
+# Parâmetros são variáveis declaradas na definição de uma função;
+# Argumentos são dados passados durante a execução de uma função;
+# A ordem dos parâmetros importa!
+
+# Argumentos nomeados (Keyword Arguments)
+
+# Caso utilizemos nomes dos parâmetros nos argumentos para informá-los,
+# podemos utilizar qualquer ordem.
+
+# Exemplo
+print(nome_completo(sobrenome='Álvaro', nome='João'))
+"""
+
+
+# Erro comum na utilização do return
+
+"""
+def soma_impares(numeros):
+    total = 0
+    for num in numeros:
+        if num % 2 != 0:
+            total += num
+        return total  # retorna apenas o primeiro número ímpar
+"""
+
+
+# Maneira correta
+
+def soma_impares(numeros):
+    total = 0
+    for num in numeros:
+        if num % 2 != 0:
+            total += num
+    return total  # retorna a soma de todos os números ímpares passados no parâmetro.
+
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(soma_impares(lista))
