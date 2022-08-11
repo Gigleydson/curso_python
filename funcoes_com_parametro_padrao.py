32480f9cb49649825677a6a35a166fbdb068246e
Gigleydson Dias <gigleydson@gmail.com>
2022-08-11 10:02:44 -0300

    Finalizando aula de funcções com parâmetro padrão

--- funcoes_com_parametro_padrao.py
+++ funcoes_com_parametro_padrao.py
@@ -47,14 +47,6 @@ def teste2(potencia, num=2):
 print(teste2(2))
 
 
-# Por quê utilizar parâmetros com valor default?
-
-- Nos permite ser mais flexíveis nas funções;
-- Evita erros com parâmetros incorretos;
-- Nos permite trabalhar com exemplos mais legíveis de código
-"""
-
-
 # Exemplo mais complexo
 
 def mostra_informacoes(nome='Geek', instrutor=False):
@@ -69,3 +61,103 @@ print(mostra_informacoes())
 print(mostra_informacoes(instrutor=True))
 print(mostra_informacoes(True))
 print(mostra_informacoes('Gigleydson'))
+
+# Por quê utilizar parâmetros com valor default?
+
+- Nos permite ser mais flexíveis nas funções;
+- Evita erros com parâmetros incorretos;
+- Nos permite trabalhar com exemplos mais legíveis de código
+
+# Quais tipos de dados podemos utilizar como valores default para parâmetros?
+
+- Qualquer tipode dado:
+    -   Números, string, floats, booleanos, listas, tuplas, dicinários, funções e etc;
+
+# Exemplos
+
+def soma(num1, num2):
+    return num1 + num2
+
+
+def subtracao(num1, num2):
+    return num1 - num2
+
+
+def mat(num1, num2, fun=soma):
+    return fun(num1, num2)
+
+
+print(soma(6, 4))
+print(mat(6, 4, subtracao))
+
+
+# Escopo - Evitar problemas e confusões...
+
+# Variáveis globais
+# Variáveis locais
+
+instrutor = 'Geek'  # Variável global
+
+
+def diz_oi():
+    # instrutor = 'Python'  # Variável local
+    return f'Oi {instrutor}'
+
+
+print(diz_oi())
+
+# OBS: Se tivermos uma variável localo com o mesmo nome de uma variável global, a local terá preferência.
+
+
+def diz_oi():
+    prof = 'Geek'  # Variável local
+    return f'Oi {prof}'
+
+
+print(diz_oi())
+
+print(prof)  # Variável só existe detro da função, por isso da o erro 'NameError'
+
+
+# ATENÇÃO com variáveis globais (Se puder evitar, evite!)
+
+total = 0
+
+
+def incrementa():
+    total = total + 1  # UnboundLocalError - Variável local sendo utilizada para processamento sem ter sido inicializada
+    return total
+
+
+print(incrementa())
+
+
+def incrementa():
+    global total  # Avisando que queremos utilizar a variável global
+    total = total + 1
+    return total
+
+
+print(incrementa())
+print(incrementa())
+print(incrementa())
+"""
+
+# Podemos ter funções que são declaradas dentro de funções, e também tem uma forma especial de escolpo de variável
+
+
+def fora():
+    contador = 0
+
+    def dentro():
+        nonlocal contador
+        contador += 1
+        return contador
+    return dentro()
+
+
+print(fora())
+print(fora())
+print(fora())
+
+print(dentro())  # NameError
