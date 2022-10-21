159803926b171e2787deabd6bd7369539a3c071a
Gigleydson Dias <gigleydson@gmail.com>
2022-10-21 18:13:45 -0300

    Aula de zip finalizada

--- zip.py
+++ zip.py
@@ -29,4 +29,31 @@ com iteráveis de tamanhos diferentes, irá parar quando os elementos do menor i
 lista3 = [7, 8, 9, 10]
 zip1 = zip(lista1, lista2, lista3)
 print(list(zip1))
+
+# Podemos utilizar diferentes iteráveis com zip.
+tupla = (1, 2, 3, 4, 5)
+lista = [6, 7, 8, 9, 10]
+dicionario = {'a': 11, 'b': 12, 'c': 13, 'd': 14, 'e': 15}
+
+zip2 = zip(tupla, lista, dicionario.values())
+print(list(zip2))
+
+# Lista de tuplas
+dados = [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5)]
+print(list(zip(*dados)))
 """
+
+# Exemplos mais complexos
+prova1 = [80, 91, 78]
+prova2 = [98, 89, 53]
+alunos = ['Maria', 'Pedro', 'Carla']
+
+final = {dado[0]: max(dado[1], dado[2]) for dado in zip(alunos, prova1, prova2)}
+
+print(final)
+
+
+# Podemos utrilizar map()
+final = zip(alunos, map(lambda nota: max(nota), zip(prova1, prova2)))
+
+print(dict(final))
